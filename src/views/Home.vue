4feabe0b12ecbb25590a10b01d07d788ba054b6b
Pawanachai <Kanpim5@gmail.com>
2020-08-09 14:29:59 +0700

    🔁: set data mockup to document

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,16 @@
     <p>{{ Profile.email }}</p>
     <br />
     <Button type="success" @click="testDB">test save</Button>
+    <Button type="error" @click="testGetDB">test save</Button>
   </div>
 </template>
 
 <script>
 // import { db } from "firebase/firebase-firestore";
 import db from "@/db";
+import data from "@/mockdata.json";
+
+// const dbDATA = db.collection("users");
 
 export default {
   name: "home-page",
@@ -26,23 +30,30 @@ export default {
     this.Profile.email = this.$store.getters.getProfileEmail;
   },
   methods: {
-    //  addReptile() {
-    //    this.$firestore.reptiles.add({
-    //      name: this.newReptile,
-    //      timestamp: new Date()
-    //    });
-    //    this.newReptile = "";
-    //  }
-    testDB() {
-      db.collection("users")
-        .doc(this.Profile.email)
-        .set(this.Profile)
-        .then(() => {
-          console.log("testDB -> this.Profile", this.Profile);
-        })
-        .catch(err => {
-          console.log("testDB -> err", err);
-        });
+    async testDB() {
+      // db.collection("users")
+      //   .add(mockData)
+      //   .then(() => {
+      //     console.log("testDB -> this.Profile", this.Profile);
+      //   })
+      //   .catch(err => {
+      //     console.log("testDB -> err", err);
+      //   });
+      await Object.keys(data).forEach(dockey => {
+        db.collection("users")
+          .doc(data[dockey].id)
+          .set(data[dockey])
+          .then(() => {
+            console.log("testDB -> this.Profile", data[dockey].id);
+          })
+          .catch(err => {
+            console.log("testDB -> err", err);
+          });
+      });
+    },
+    async testGetDB() {
+      const snapshot = await db.collection("users").get();
+      console.log(snapshot.docs.map(doc => doc.data()));
     }
   }
 };
